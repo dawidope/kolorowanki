06c74a40cc31cab8d9f17b596281c03aece37435
Dawid Wenderski <dawidope@gmail.com>
2026-08-26 16:01:16 +0200

    Przelacznik diagnostyki trafienia w pasku gornym
    Krzyzyk i ramka zasiegu sa teraz opcjonalne i domyslnie wylaczone. Wylacznik zdejmuje
    tez pelny skan mapy etykiet (4,6 mln iteracji na watku UI przy kazdym tapnieciu), a nie
    tylko samo rysowanie.

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"e4b8dca3f1b4ede4c30371002441c88c12187e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "706257714" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3664415779" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
